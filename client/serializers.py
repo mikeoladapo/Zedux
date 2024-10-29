60446bdb741e4b5dd3a8b4e06d70d7f0c24fe66f
lionheart <mikeoladapo2004@gmail.com>
2024-10-29 15:48:13 +0100

    modified

--- client/serializers.py
+++ client/serializers.py
@@ -4,9 +4,9 @@ from .models import MyCart , MyCourse
 class MyCartSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyCart
-        fields = ['id', 'user', 'course_name']
+        fields = ['id','course_name']
 
 class MyCourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyCourse
-        fields = ['id', 'user', 'course_name', 'bought_on']
+        fields = ['id','course_name', 'bought_on']

c49b0a1fd86d560bf9783e5999d15a6eeae147f4
lionheart <mikeoladapo2004@gmail.com>
2024-10-31 17:15:39 +0100

    modified

--- Zedux/settings.py
+++ Zedux/settings.py
@@ -128,3 +128,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}

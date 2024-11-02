b3794bc9c0df311187d43f965d84e6e3f6f5cab6
lionheart <mikeoladapo2004@gmail.com>
2024-11-02 11:39:12 +0100

    modified

--- Zedux/settings.py
+++ Zedux/settings.py
@@ -130,7 +130,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }

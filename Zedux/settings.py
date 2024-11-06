f3327740d4f301602a43e26d0bff11cf7e5d89ab
lionheart <mikeoladapo2004@gmail.com>
2024-11-06 20:23:11 +0100

    modified

--- Zedux/settings.py
+++ Zedux/settings.py
@@ -139,8 +139,15 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
+}
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user': 'base.serializers.CustomUserSerializer',         
+        'current_user': 'base.serializers.CustomUserSerializer',
+    },
 }

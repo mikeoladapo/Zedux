1aa9445763510165edbee1046eb0e1c4f89551e4
lionheart <mikeoladapo2004@gmail.com>
2024-11-15 20:49:48 +0100

    modified

--- Zedux/settings.py
+++ Zedux/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-12345abcdef')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://zedux-1.onrender.com', 'localhost']
+ALLOWED_HOSTS =['*']#['https://zedux-1.onrender.com', 'localhost']
 
 AUTH_USER_MODEL = 'base.CustomUser'
 

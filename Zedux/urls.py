e370dcbb13d9d494e16a94d2af85cc1b723e35cc
lionheart <mikeoladapo2004@gmail.com>
2025-10-25 14:14:25 +0100

    latest

--- Zedux/urls.py
+++ Zedux/urls.py
@@ -19,7 +19,7 @@ from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpattern
 
 urlpatterns = [
     path('admin/', admin.site.urls),
